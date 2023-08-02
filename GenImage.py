978f3a315d2fd748b8f600b802b3c338328e417e
Sasi Kanth <92316448+Sasikanth-17@users.noreply.github.com>
2023-08-02 13:33:34 +0530

    Update GenImage.py

--- GenImage.py
+++ GenImage.py
@@ -2,7 +2,7 @@ def t2img(input2):
     import requests
     
     API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
-    headers = {"Authorization": "Bearer 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"}
+    headers = {"Authorization": "Bearer API_KEY"}
     
     def query(payload):
     	response = requests.post(API_URL, headers=headers, json=payload)
